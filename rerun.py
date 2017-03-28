6a9243b7dbddbc01db876b9f61751ba0432ff3d6
William Ehlhardt <williamehlhardt@gmail.com>
2017-03-28 02:38:40 -0700

    Sketch out an "ops" struct to hand around.
    It's a bit awkward that each state has to hand its ops to its successor state. Ideally, we'd have the main loop call the successor's constructor, but it's hard to tell whether a state returned itself or some new state that needs to be initialized.

--- rerun.py
+++ rerun.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import collections
 import Queue
 
 PROCEND  = 'PROCEND'
@@ -7,7 +8,15 @@ MODIFY   = 'MODIFY'
 TIMEDONE = 'TIMEDONE'
 EVENTS   = set([PROCEND, MODIFY, TIMEDONE])
 
+Ops = collections.namedtuple(
+    'Ops',
+    ['start_timer',
+     'start_tests',
+     'send_kill'])
+
 class State:
+    def __init__(self, ops):
+        self.ops = ops
     def modify(self):
         raise NotImplementedError()
     def timedone(self):
@@ -17,42 +26,48 @@ class State:
 
 class IdleState(State):
     def modify(self):
-        return DebounceState()
+        return DebounceState(self.ops)
 
 class DebounceState(State):
-    def __init__(self):
-        spawn_timer()
+    def __init__(self, ops):
+        # TODO: Derp, how do I inherit?
+        State.__init__(self, ops)
+
+        self.ops.start_timer()
 
     def modify(self):
         return self
 
     def timedone(self):
-        return TestingState()
+        return TestingState(self.ops)
 
 class TestingState(State):
     def __init__(self):
-        self.pid = spawn_tests()
+        # TODO: Derp, how do I inherit?
+        State.__init__(self, ops)
+
+        self.pid = self.ops.start_tests()
 
     def modify(self):
-        send_kill(self.pid)
-        return KillWaitState()
+        self.ops.send_kill(self.pid)
+        return KillWaitState(self.ops)
 
     def procend(self):
         # TODO: Capture output and print it here?
-        return IdleState()
+        return IdleState(self.ops)
 
 class KillWaitState(State):
     def modify(self):
         return self
 
     def procend(self):
-        return TestingState()
+        return TestingState(self.ops)
 
         
 def run(self):
     events = Queue.Queue()
-    spawn_inotify(self.events)
-    state = IdleState()
+    spawn_inotify(events)
+    state = IdleState(ops)
 
     while True:
         event = events.get()
